f89e0d2c42d9f6dae7865c368807aeb6a293427d
Chang <cnxiong03@gmail.com>
2021-03-05 18:46:16 -0500

    solving algorithm

---
+++ src/com/codewithchang/Main.java
@@ -0,0 +1,38 @@
+package com.codewithchang;
+
+public class Main {
+
+    public static void main(String[] args) {
+	// write your code here
+    }
+
+
+
+//
+// Input = Array of integers that represent the height of apartments. Left most apartment is in
+// index 0 & the array lists the apartments from left to right. After the rightmost apartment,
+// we hit the ocean. Apartments are of the same width and in a straight line from left to right towards the ocean.
+// Output = Take in the Array and return the indexes of the apartments that have ocean view
+
+    // Ocean View
+// Input: [4, 3, 2, 3, 1]
+// Output: [0, 3, 4]
+//
+//   ___
+// 4 |  |  ___         ___
+// 3 |  |  |  |  ___   |  |
+// 2 |  |  |  |  |  |  |  |  ___
+// 1 |  |  |  |  |  |  |  |  |  |
+//                              ~~~~~ Ocean
+//   b0,4  b1,3  b2,2  b3,3  b4 1
+//    static int[] getOceanView(int[] bldheiths)
+//    {
+//
+//        return r;
+//    }
+//Most efficient solution
+//How can we improve eficiency of solution of result if solution not satisfactory
+//Understanding the complexity and big O of a function
+//A software engineer is good at understanding problems and good at solving problems and creating solutions
+// in the shortest time possible
+}
